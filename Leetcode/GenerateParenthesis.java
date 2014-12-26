51267e80d4d43e6395cc93b3d9496660945b536e
GambleGear <zhushiyao.zsy@gmail.com>
2014-12-26 13:25:51 -0500

    Update GenerateParenthesis.java

--- Leetcode/GenerateParenthesis.java
+++ Leetcode/GenerateParenthesis.java
@@ -9,6 +9,48 @@ For example, given n = 3, a solution set is:
 // Judge the loop termination conditions
 // Separarte the situation into left and right to consider.
 
+/* Iteration */
+
+class Element{
+    String str;
+    int left;
+    int right;
+    
+    public Element(String s, int l, int r){
+        str=s;
+        left=l;
+        right=r;
+    }
+}
+
+public class Solution {
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> al=new ArrayList<String>();
+        
+        Queue<Element> q=new LinkedList<Element>();
+        q.add(new Element("",0,0));
+        while(!q.isEmpty()){
+            Element e=q.poll();
+            if(e.left==n && e.right==n){
+                al.add(e.str);
+            }
+            else{
+                if(e.left<n)
+                    q.add(new Element(e.str+"(", e.left+1,e.right));
+                
+                if(e.left>e.right)
+                    q.add(new Element(e.str+")",e.left,e.right+1));
+            }
+        }
+        
+        return al;
+    } 
+}
+
+
+/* Recursion */
+
+
 public class Solution {
     public ArrayList<String> generateParenthesis(int n) {
         // IMPORTANT: Please reset any member data you declared, as
