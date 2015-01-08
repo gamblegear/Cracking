7e636ba7a9a886cc12e3cbc6c2062bf8a9612f2f
GambleGear <zhushiyao.zsy@gmail.com>
2015-01-08 08:02:05 -0500

    Update Sqrt.java

--- Leetcode/Sqrt.java
+++ Leetcode/Sqrt.java
@@ -5,27 +5,19 @@ Each step is 1 length long.
 
 public class Solution {
     public int sqrt(int x) {
-        if (x <= 0) {
-            return 0;
+        long i = 0;
+        long j = x/2+1;
+        
+        while(i <= j) {
+            long mid = (i + j)/2;
+            if(mid * mid == x)
+                return (int)mid;
+            else if(mid * mid < x)
+                i = mid + 1;
+            else
+                j = mid - 1;
         }
-    
-        long low = 1;
-        long high = x;
-        while (low < high) {
-            long mid = (low + high) / 2;
-            if (mid * mid > (long)x) {
-                if ((mid - 1) * (mid - 1) < (long)x) {
-                    return (int)mid - 1;
-                }
-                high = mid - 1;
-            }
-            else {
-                if ((mid + 1) * (mid + 1) > (long)x) {
-                    return (int)mid;
-                }
-                low = mid + 1;
-            }
-        }
-        return (int)low;
+        
+        return (int)j;
     }
 }
