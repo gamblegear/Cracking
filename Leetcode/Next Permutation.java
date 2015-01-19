21a6759b177790821f71ca5f9316ca78b48179c3
GambleGear <zhushiyao.zsy@gmail.com>
2015-01-19 10:30:27 -0500

    Update Next Permutation.java

--- Leetcode/Next Permutation.java
+++ Leetcode/Next Permutation.java
@@ -1,3 +1,5 @@
+// this is the easier solution
+
 public class Solution {
     //http://blog.csdn.net/linhuanmars/article/details/20434115
     /*
@@ -31,4 +33,62 @@ public class Solution {
         while(i < j)  
             swap(num, i++, j--);  
     }
+}
+
+better solution:
+
+public class Solution {
+    public void nextPermutation(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int len = num.length;
+    	if(len<=1) return;
+		int i= len-1;
+		while(i>0){
+			if(num[i]<=num[i-1])
+				i--;
+			else
+				break;
+		}
+		if(i==0){
+			reverse(num);
+			return;
+		}
+		
+		int p = i-1;
+		while(i<len){
+			if(num[i]>num[p])
+				i++;
+			else
+				break;
+		}
+        i--;
+		swap(num, p, i);
+		p++;
+		int q = len-1;
+		while(q>p){
+			swap(num,p,q);
+			p++;
+			q--;
+		}
+		
+
+    }
+    
+    public void swap(int[] num, int i, int j){
+		int temp = num[i];
+		num[i] = num[j];
+		num[j] = temp;       
+    }
+
+	public void reverse(int[] num){
+		if(num.length==1) return;
+		int p = 0;
+		int q = num.length-1;
+		while(q>p){
+			swap(num,p,q);
+			p++;
+			q--;
+		}
+	}
 }
