75187b61fe98e7337eaf0807b22ed73e254d75c6
GambleGear <zhushiyao.zsy@gmail.com>
2016-07-05 08:52:52 -0400

    Update LargestDivisibleSubset.java

--- Leetcode/LargestDivisibleSubset.java
+++ Leetcode/LargestDivisibleSubset.java
@@ -55,3 +55,47 @@ public class Solution {
 // to add
 // check eligible
 // compare size
+
+//This is the DP solution
+
+/*
+A few key points here are:
+1. Not try to directly keep track of the largest subset - understand the meaning of subset and only keeps the largest element
+2. When subset/substring - think about DP
+*/
+
+public class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) return res;
+        Arrays.sort(nums);
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        
+        //for each element in nums, find the length of largest subset it has.
+        for (int i = 1; i < nums.length; i++){
+            for (int j = i-1; j >= 0; j--){
+                if (nums[i] % nums[j] == 0){
+                    dp[i] = dp[j] + 1;
+                    break;
+                }
+            }
+        }
+        
+        //pick the index of the largest element in dp.
+        int maxIndex = 0;
+        for (int i = 1; i < nums.length; i++){
+            maxIndex = dp[i] > dp[maxIndex] ?  i :  maxIndex;
+        }
+        
+        //from nums[maxIndex] to 0, add every element belongs to the largest subset.
+        int temp = nums[maxIndex];
+        for (int i = maxIndex; i >= 0; i--){
+            if (temp % nums[i] == 0){
+                res.add(nums[i]);
+                temp = nums[i];
+            }
+        }
+        return res;
+    }
+}
